9b6881e1bb6a77b582c6327fb0c9233901e1f613
ten2ten2 <lightning007zz@gmail.com>
2025-05-31 16:03:37 +0800

    comment unused code

--- src/components/GoogleAdSense.tsx
+++ src/components/GoogleAdSense.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { hasUserDeclinedCookies } from './CookieConsent';
+// import { hasUserDeclinedCookies } from './CookieConsent';
 
 // Declare global adsbygoogle
 declare global {
